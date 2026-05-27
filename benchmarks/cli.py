a344f2c57c514548b41ab96fdf0cb2da48872593
Mr-Leshiy <leshiy12345678@gmail.com>
2026-05-27 08:56:42 +0200

    cleaned benchmarks cli parsing

--- benchmarks/cli.py
+++ benchmarks/cli.py
@@ -1,9 +1,9 @@
 import typer
 
-from benchmarks.block_ciphers.aes.cli import app as aes_app
+from benchmarks.block_ciphers.cli import app as block_ciphers_app
 
 app = typer.Typer(help="mojo-crypto benchmarks.")
-app.add_typer(aes_app, name="aes")
+app.add_typer(block_ciphers_app, name="block_ciphers")
 
 if __name__ == "__main__":
     app()
